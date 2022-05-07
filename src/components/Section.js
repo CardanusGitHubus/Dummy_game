f2da483eda60d3d557d70fd92c67a9cf8fd093b6
Dmitry Konychev <dalexeych@gmail.com>
2022-05-08 02:25:34 +0300

    fix: chat order

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,13 +2,16 @@
 // no deletion no nothing
 
 export default class Section {
-  constructor(selector) {
+  constructor(selector, backwards) {
+    this._backwards = backwards;
     this._selector = selector;
     this._containerElement = document.querySelector(this._selector);
   }
 
   add(...elements){
-    elements.forEach(_ => this._containerElement.prepend(_));
+    this._backwards
+      ? elements.forEach(_ => this._containerElement.prepend(_))
+      : elements.forEach(_ => this._containerElement.append(_));
   }
 
   clear(){
